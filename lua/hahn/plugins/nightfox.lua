95ac54109104c2912ab73ec89a8f8c0410d84852
Leonardo Hahn <leonnardo.hahn@gmail.com>
2023-02-13 19:14:31 -0300

    Update plugins

--- lua/hahn/plugins/nightfox.lua
+++ lua/hahn/plugins/nightfox.lua
@@ -2,6 +2,7 @@ return {
     "EdenEast/nightfox.nvim",
     dependencies = {
         "RRethy/vim-illuminate",
+        "nvim-lualine/lualine.nvim",
     },
     config = function()
         require("nightfox").setup {
---
+++ lua/hahn/plugins/null_ls.lua
@@ -0,0 +1,19 @@
+return {
+	"jose-elias-alvarez/null-ls.nvim",
+
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting
+		null_ls.setup({
+			sources = {
+				formatting.black.with({ extra_args = { "--fast" } }),
+				formatting.prettierd,
+				formatting.stylua,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>bf", function()
+			vim.lsp.buf.format()
+		end)
+	end,
+}
