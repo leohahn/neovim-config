5e5eb053a38696a2b38399a440469068a4cc9c43
Leonardo Hahn <leonnardo.hahn@gmail.com>
2023-10-05 19:29:24 -0300

    feat: better treesitter context config

--- lua/hahn/plugins/treesitter-context.lua
+++ lua/hahn/plugins/treesitter-context.lua
@@ -3,8 +3,9 @@ return {
     config = function()
         local treesitter_context = require("treesitter-context")
         treesitter_context.setup({
-            max_lines = 0,
+            max_lines = 1,
             separator = nil,
+            trim_scope = 'inner', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
         })
     end
 }
