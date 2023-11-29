8fb2fd033177699893ef4d3028b916f80f3f1045
Leonardo Hahn <leonnardo.hahn@gmail.com>
2023-11-29 16:25:06 -0300

    feat: add conform for formatting files

--- lua/hahn/plugins/null_ls.lua
+++ lua/hahn/plugins/null_ls.lua
@@ -3,18 +3,20 @@ return {
 
 	config = function()
 		local null_ls = require("null-ls")
-		local formatting = null_ls.builtins.formatting
+		-- local formatting = null_ls.builtins.formatting
 		null_ls.setup({
 			sources = {
-				formatting.black.with({ extra_args = { "--fast" } }),
-				formatting.prettierd,
-				formatting.stylua,
-				formatting.golines,
+				-- formatting.black.with({ extra_args = { "--fast" } }),
+				-- formatting.prettierd,
+				-- formatting.stylua,
+				-- formatting.golines,
 			},
 		})
 
 		vim.keymap.set("n", "<leader>bf", function()
+			vim.cmd("Format")
 			vim.lsp.buf.format()
+			-- require("conform").format()
 		end)
 	end,
 }
